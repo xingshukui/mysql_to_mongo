f2ba53d8c4e58fade92427eceff6f068c8aae56b
xingshukui <xingshukui@163.com>
2018-08-22 10:15:03 +0800

    host修改

--- src/main/java/com/bajiuling/Constant.java
+++ src/main/java/com/bajiuling/Constant.java
@@ -8,7 +8,7 @@ package com.bajiuling;
  */
 public final class Constant {
 
-    public static final String MONGO_URL = "mongodb://jihepai_dev:890jihepai@dds-bp17f7d5150e54641295-pub.mongodb.rds.aliyuncs.com:3717,dds-bp17f7d5150e54642378-pub.mongodb.rds.aliyuncs.com:3717/jihepai_dev?readPreference=nearest&replicaSet=mgset-8009723";
+    public static final String MONGO_URL = "mongodb://jihepai_dev:890jihepai@localhost:27017/jihepai_dev?readPreference=nearest";
     public static final String MONGO_HOST = "";
     public static final String MONGO_PORT = "";
     public static final String MONGO_COLLECTION = "";
@@ -17,7 +17,7 @@ public final class Constant {
 
 
     public static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
-    public static final String MYSQL_URL = "jdbc:mysql://rm-bp1qob5fyq5t910g25o.mysql.rds.aliyuncs.com:3306/jihepai_dev?allowMultiQueries=true&useUnicode=true&characterEncoding=UTF-8";
+    public static final String MYSQL_URL = "jdbc:mysql://localhost:3306/jihepai_dev?allowMultiQueries=true&useUnicode=true&characterEncoding=UTF-8";
     public static final String MYSQL_NAME = "jihepai_dev";
     public static final String MYSQL_PASSWORD = "jihepai_dev";
 
